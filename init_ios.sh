0cba8e395ec3036213818b885ca8a0f93ad3a9ed
dargon <ptutino@gmail.com>
2014-12-23 22:05:09 -0800

    Added plugins at ios initialization: init_ios.h

--- init_ios.sh
+++ init_ios.sh
@@ -4,12 +4,14 @@ cordova plugin add org.apache.cordova.device
 cordova plugin add https://github.com/driftyco/ionic-plugins-keyboard.git
 cordova plugin add https://github.com/brodysoft/Cordova-SQLitePlugin.git
 #cordova plugin add https://github.com/wildabeast/BarcodeScanner.git
-cordova plugin add https://github.com/jonathannaguin/BarcodeScanner.git
+#cordova plugin add https://github.com/jonathannaguin/BarcodeScanner.git
+cordova plugin add https://github.com/latincoin-com/BarcodeScanner.git
 cordova plugin add https://github.com/VersoSolutions/CordovaClipboard
 cordova plugin add https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin.git
 cordova plugin add org.apache.cordova.splashscreen
 cordova plugin add https://github.com/EddyVerbruggen/Toast-PhoneGap-Plugin.git
 cordova plugin add org.apache.cordova.globalization
+cordova plugin add https://github.com/latincoin-com/cordova-plugin-bitwallet.git
 cp resources/ios/icons/icon-small@2x.png platforms/ios/Bitwallet/Resources/icons/
 cp resources/ios/icons/icon-small.png platforms/ios/Bitwallet/Resources/icons/
 cp resources/ios/icons/icon-76@2x.png platforms/ios/Bitwallet/Resources/icons/
--- plugins_ios.sh
+++ plugins_ios.sh
@@ -3,9 +3,11 @@ cordova plugin add org.apache.cordova.device
 cordova plugin add https://github.com/driftyco/ionic-plugins-keyboard.git
 cordova plugin add https://github.com/brodysoft/Cordova-SQLitePlugin.git
 #cordova plugin add https://github.com/wildabeast/BarcodeScanner.git
-cordova plugin add https://github.com/jonathannaguin/BarcodeScanner.git
+#cordova plugin add https://github.com/jonathannaguin/BarcodeScanner.git
+cordova plugin add https://github.com/latincoin-com/BarcodeScanner.git
 cordova plugin add https://github.com/VersoSolutions/CordovaClipboard
 cordova plugin add https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin.git
 cordova plugin add org.apache.cordova.splashscreen
 cordova plugin add https://github.com/EddyVerbruggen/Toast-PhoneGap-Plugin.git
 cordova plugin add org.apache.cordova.globalization
+cordova plugin add https://github.com/latincoin-com/cordova-plugin-bitwallet.git
