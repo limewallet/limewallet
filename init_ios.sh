29d2fd3173b16535535b101bbff39a86a342a22f
dargon <ptutino@gmail.com>
2015-06-25 17:21:33 -0700

    iOS changes

--- init_ios.sh
+++ init_ios.sh
@@ -3,7 +3,7 @@ rm -rf plugins/*
 cordova platform add ios --usenpm
 cordova plugin add org.apache.cordova.console
 cordova plugin add org.apache.cordova.device
-cordova plugin add https://github.com/apache/cordova-plugin-whitelist.git
+#cordova plugin add https://github.com/apache/cordova-plugin-whitelist.git
 cordova plugin add https://github.com/driftyco/ionic-plugins-keyboard.git
 cordova plugin add https://github.com/brodysoft/Cordova-SQLitePlugin.git
 cordova plugin add https://github.com/latincoin-com/BarcodeScanner.git
