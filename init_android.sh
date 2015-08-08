ddfbf422fc3bf1d2ae68c102a331468d77b4f3b5
Matias Romeo <matias.romeo@gmail.com>
2015-08-08 17:54:24 -0300

    Use new plugin names

--- init_android.sh
+++ init_android.sh
@@ -1,17 +1,17 @@
-rm -rf platforms/android
-rm -rf plugins/*
-cordova platform add android@4.0.0 --usenpm
-cordova plugin add org.apache.cordova.console
-cordova plugin add org.apache.cordova.device
+rm -rf platforms
+rm -rf plugins
+cordova platform add android
+cordova plugin add cordova-plugin-console
+cordova plugin add cordova-plugin-device
+cordova plugin add cordova-plugin-splashscreen
+cordova plugin add cordova-plugin-globalization
 cordova plugin add https://github.com/apache/cordova-plugin-whitelist.git
 cordova plugin add https://github.com/driftyco/ionic-plugins-keyboard.git
 cordova plugin add https://github.com/brodysoft/Cordova-SQLitePlugin.git
-cordova plugin add https://github.com/latincoin-com/BarcodeScanner.git
+cordova plugin add https://github.com/limewallet/BarcodeScanner.git
 cordova plugin add https://github.com/VersoSolutions/CordovaClipboard
 cordova plugin add https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin.git
-cordova plugin add org.apache.cordova.splashscreen
 cordova plugin add https://github.com/EddyVerbruggen/Toast-PhoneGap-Plugin.git
-cordova plugin add org.apache.cordova.globalization
-cordova plugin add https://github.com/latincoin-com/cordova-plugin-bitwallet.git
-cordova plugin add https://github.com/latincoin-com/Custom-URL-scheme.git --variable URL_SCHEME=bts
+cordova plugin add https://github.com/limewallet/cordova-plugin-bitwallet.git
+cordova plugin add https://github.com/limewallet/Custom-URL-scheme.git --variable URL_SCHEME=bts
 
