6f9c8bcb8a74c3830e1c64fa4b730e784bd2c0c0
Matias Romeo <matias.romeo@gmail.com>
2015-08-08 17:56:38 -0300

    Fix relative plugin paths

--- update_plugin.sh
+++ update_plugin.sh
@@ -1,18 +1,18 @@
-cp ../cordova-plugin-bitwallet/www/BitsharesPlugin.js ./plugins/com.latincoin.BitsharesPlugin/www/BitsharesPlugin.js
-cp ../cordova-plugin-bitwallet/src/android/BitsharesPlugin.java ./plugins/com.latincoin.BitsharesPlugin/src/android/BitsharesPlugin.java
-cp ../cordova-plugin-bitwallet/src/android/BitsharesPlugin_impl.java ./plugins/com.latincoin.BitsharesPlugin/src/android/BitsharesPlugin_impl.java
-cp ../cordova-plugin-bitwallet/src/android/BlockCypherTestnetParams.java ./plugins/com.latincoin.BitsharesPlugin/src/android/BlockCypherTestnetParams.java
-cp ../cordova-plugin-bitwallet/src/android/Crypto.java ./plugins/com.latincoin.BitsharesPlugin/src/android/Crypto.java 
-cp ../cordova-plugin-bitwallet/src/android/Skip32.java ./plugins/com.latincoin.BitsharesPlugin/src/android/Skip32.java 
+cp ../cordova-plugin-limewallet/www/BitsharesPlugin.js ./plugins/com.latincoin.BitsharesPlugin/www/BitsharesPlugin.js
+cp ../cordova-plugin-limewallet/src/android/BitsharesPlugin.java ./plugins/com.latincoin.BitsharesPlugin/src/android/BitsharesPlugin.java
+cp ../cordova-plugin-limewallet/src/android/BitsharesPlugin_impl.java ./plugins/com.latincoin.BitsharesPlugin/src/android/BitsharesPlugin_impl.java
+cp ../cordova-plugin-limewallet/src/android/BlockCypherTestnetParams.java ./plugins/com.latincoin.BitsharesPlugin/src/android/BlockCypherTestnetParams.java
+cp ../cordova-plugin-limewallet/src/android/Crypto.java ./plugins/com.latincoin.BitsharesPlugin/src/android/Crypto.java 
+cp ../cordova-plugin-limewallet/src/android/Skip32.java ./plugins/com.latincoin.BitsharesPlugin/src/android/Skip32.java 
 
-cp ../cordova-plugin-bitwallet/assets/english.txt ./plugins/com.latincoin.BitsharesPlugin/assets/english.txt
+cp ../cordova-plugin-limewallet/assets/english.txt ./plugins/com.latincoin.BitsharesPlugin/assets/english.txt
 
-cp ../cordova-plugin-bitwallet/www/BitsharesPlugin.js ./platforms/android/assets/www/plugins/com.latincoin.BitsharesPlugin/www/BitsharesPlugin.js
-cp ../cordova-plugin-bitwallet/src/android/BitsharesPlugin.java ./platforms/android/src/com/latincoin/bitwallet/BitsharesPlugin.java
-cp ../cordova-plugin-bitwallet/src/android/BitsharesPlugin_impl.java ./platforms/android/src/com/latincoin/bitwallet/BitsharesPlugin_impl.java
-cp ../cordova-plugin-bitwallet/src/android/BlockCypherTestnetParams.java ./platforms/android/src/com/latincoin/bitwallet/BlockCypherTestnetParams.java
+cp ../cordova-plugin-limewallet/www/BitsharesPlugin.js ./platforms/android/assets/www/plugins/com.latincoin.BitsharesPlugin/www/BitsharesPlugin.js
+cp ../cordova-plugin-limewallet/src/android/BitsharesPlugin.java ./platforms/android/src/com/latincoin/limewallet/BitsharesPlugin.java
+cp ../cordova-plugin-limewallet/src/android/BitsharesPlugin_impl.java ./platforms/android/src/com/latincoin/limewallet/BitsharesPlugin_impl.java
+cp ../cordova-plugin-limewallet/src/android/BlockCypherTestnetParams.java ./platforms/android/src/com/latincoin/limewallet/BlockCypherTestnetParams.java
 
-cp ../cordova-plugin-bitwallet/src/android/Crypto.java ./platforms/android/src/de/schildbach/wallet/util/Crypto.java
-cp ../cordova-plugin-bitwallet/src/android/Skip32.java ./platforms/android/src/com/boivie/skip32/Skip32.java
+cp ../cordova-plugin-limewallet/src/android/Crypto.java ./platforms/android/src/de/schildbach/wallet/util/Crypto.java
+cp ../cordova-plugin-limewallet/src/android/Skip32.java ./platforms/android/src/com/boivie/skip32/Skip32.java
 
-cp ../cordova-plugin-bitwallet/assets/english.txt ./platforms/android/assets/english.txt
+cp ../cordova-plugin-limewallet/assets/english.txt ./platforms/android/assets/english.txt
