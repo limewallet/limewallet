a7f160a9daa7d5380cf56bfacdbe40915da84567
Matias Romeo <matias.romeo@gmail.com>
2015-08-08 23:45:16 -0300

    Add bitEUR and bitSILVER

--- www/js/config/environment-prod.config.js
+++ www/js/config/environment-prod.config.js
@@ -4,10 +4,12 @@ bitwallet_config
   timeout       : 10000,
   default_asset : 22,
   assets : [
-    {id: 22,  name: 'bitUSD',  symbol: 'USD',  unit: 'USD',  fullname: 'United States Dollar', precision: 10000 ,    symbol_ui_text : '$'},
-    {id:  7,  name: 'bitGOLD', symbol: 'GOLD', unit: 'OZ',   fullname: 'Gold' ,                precision: 1000000,   symbol_ui_text : 'oz'},
-    {id: 14,  name: 'bitCNY',  symbol: 'CNY',  unit: 'CNY',  fullname: 'Chinese Yuan' ,        precision: 10000,     symbol_ui_text : '¥'},
-    {id:  4,  name: 'bitBTC',  symbol: 'BTC',  unit: 'BTC',  fullname: 'Bitcoin' ,             precision: 100000000, symbol_ui_text : '€'}
+    {id: 22,  name: 'bitUSD',    symbol: 'USD',    unit: 'USD',  fullname: 'United States Dollar', precision: 10000 ,    symbol_ui_text : '$' },
+    {id:  7,  name: 'bitGOLD',   symbol: 'GOLD',   unit: 'OZ',   fullname: 'Gold' ,                precision: 1000000,   symbol_ui_text : 'oz'},
+    {id:  6,  name: 'bitSILVER', symbol: 'SILVER', unit: 'OZ',   fullname: 'Silver' ,              precision: 10000,     symbol_ui_text : 'oz'},
+    {id: 21,  name: 'bitEUR',    symbol: 'EUR',    unit: 'EUR',  fullname: 'Euro' ,                precision: 10000,     symbol_ui_text : '€' },
+    {id: 14,  name: 'bitCNY',    symbol: 'CNY',    unit: 'CNY',  fullname: 'Chinese Yuan' ,        precision: 10000,     symbol_ui_text : '¥' },
+    {id:  4,  name: 'bitBTC',    symbol: 'BTC',    unit: 'BTC',  fullname: 'Bitcoin' ,             precision: 100000000, symbol_ui_text : 'Ƀ' }
   ],
   apiurl : function(path) {
     var url = 'https://api.limewallet.io/api/v2';
