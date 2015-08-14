44f1fb74a0deb03b1ef2aa861d4c1740d29ae316
Matias Romeo <matias.romeo@gmail.com>
2015-08-14 20:44:51 -0300

    Upgrade schema to v13

--- www/js/config/db-schema.config.js
+++ www/js/config/db-schema.config.js
@@ -1,11 +1,28 @@
 bitwallet_config
 .constant('DB_CONFIG', {
-    name: 'wallet.db',
-    version : 1.2,
-    tables: [
+    name    : 'wallet.db',
+    version : 13,
+    upgrades: {
+      '12-13' : [
+        'ALTER TABLE memo ADD COLUMN name text default null',
+        'ALTER TABLE memo ADD COLUMN to_pubkey default null',
+        'ALTER TABLE memo ADD COLUMN decrypt_error integer default 0',
+        'DELETE FROM memo',
+        "INSERT or REPLACE into setting (name, value) values ('schema_version',13)",
+        "CREATE INDEX inx_memo_address on memo (address)",
+        "CREATE INDEX inx_memo_pubkey on memo (pubkey)",
+        "CREATE INDEX inx_operation_txid on operation (txid)",
+        "CREATE INDEX inx_operation_memo_hash on operation (memo_hash)",
+        "CREATE INDEX inx_operation_address on operation (address)",
+        "CREATE INDEX inx_exchange_transaction_cl_pay_tx on exchange_transaction (cl_pay_tx)",
+        "CREATE INDEX inx_exchange_transaction_cl_recv_tx on exchange_transaction (cl_recv_tx)",
+        "CREATE INDEX inx_exchange_transaction_extra_data on exchange_transaction (extra_data)"
+      ]
+    },
+    tables  : [
         {
-            name: 'account',
-            columns: [
+            name    : 'account',
+            columns : [
                 { name : 'id',                type  : 'integer primary key'},
                 { name : 'name',              type  : 'text unique not null'},
                 { name : 'address',           type  : 'text unique not null'},
@@ -25,47 +42,53 @@ bitwallet_config
                 { name : 'access_key',        type  : 'text unique not null'},
                 { name : 'secret_key',        type  : 'text unique not null'},
                 { name : 'created_at',        type  : 'datetime default CURRENT_TIMESTAMP'}
-            ]
+            ],
+            indexes : []
         },
         {
-            name: 'setting',
-            columns: [
+            name    : 'setting',
+            columns : [
                 { name : 'name',              type   : 'text primary key'},
                 { name : 'value',             type   : 'text'}
-            ]
+            ],
+            indexes : []
         },
         {
-            name: 'contact',
-            columns: [
+            name    : 'contact',
+            columns : [
                 { name : 'id',                type  : 'integer primary key'},
                 { name : 'name',              type  : 'text unique not null'}, 
                 { name : 'address',           type  : 'text unique'},
                 { name : 'pubkey',            type  : 'text unique'},
                 { name : 'source',            type  : 'text not null'},
-                //{ name : 'public_data',       type  : 'text'},
                 { name : 'avatar_hash',       type  : 'text'},
                 { name : 'created_at',        type  : 'datetime default CURRENT_TIMESTAMP'},
                 { name : 'backup',            type  : 'integer default 0'}
-            ]
+            ],
+            indexes : []
         },
         {
-            name: 'memo',
-            columns: [
+            name    : 'memo',
+            columns : [
                 { name : 'id',                  type  : 'text primary key'},
                 { name : 'account',             type  : 'integer not null'}, 
                 { name : 'encrypted',           type  : 'integer default 1'}, 
                 { name : 'memo',                type  : 'text'}, 
                 { name : 'one_time_key',        type  : 'text'}, 
                 { name : 'message',             type  : 'text'}, 
+                { name : 'name',                type  : 'text default null'}, //v13
+                { name : 'decrypt_error',       type  : 'integer default 0'}, //v13
                 { name : 'pubkey',              type  : 'text'},
+                { name : 'to_pubkey',           type  : 'text default null'}, //v13
                 { name : 'in_out',              type  : 'integer not null'},
                 { name : 'slate',               type  : 'integer'},
                 { name : 'address',             type  : 'text'}
-            ]
+            ],
+            indexes : ['address','pubkey']
         },
         {
-            name: 'operation',
-            columns: [
+            name    : 'operation',
+            columns : [
                 { name : 'block_id'  , type: 'text'},
                 { name : 'timestamp' , type: 'integer'},
                 { name : 'memo_hash' , type: 'text'},
@@ -78,11 +101,11 @@ bitwallet_config
                 { name : 'type'      , type: 'text'},
                 { name : 'slate'     , type: 'integer'}
             ],
-            indexes: ['txid']
+            indexes : ['txid','memo_hash','address']
         },
         {
-            name: 'exchange_transaction',
-            columns: [
+            name    : 'exchange_transaction',
+            columns : [
                 { name : 'id',                     type   : 'integer primary key'},
                 { name : 'asset_id',               type   : 'integer'},
                 { name : 'cl_pay',                 type   : 'text'},
@@ -104,16 +127,17 @@ bitwallet_config
                 { name : 'cl_cmd',                 type   : 'text'},
                 { name : 'created_at',             type   : 'integer'}
             ],
-            indexes: ['cl_pay_tx', 'cl_recv_tx']
+            indexes : ['cl_pay_tx','cl_recv_tx','extra_data']
         },
         {
-            name: 'balance',
-            columns: [
+            name    : 'balance',
+            columns : [
                 {name : 'id',                     type   : 'string primary key'},
                 {name : 'address',                type   : 'string'},
                 {name : 'amount',                 type   : 'integer'},
                 {name : 'asset_id',               type   : 'integer'},
-          ]
+            ],
+            indexes : []
         }
     ],
 });
